0fe86a6ddfa67a2820beb8bcdff0833312073b77
Alex St <alexpro45.dev@yandex.ru>
2017-05-06 07:28:53 +0300

    fix greedy parser pattern

--- web-spyder/src/main/java/ru/alex/st/hh/disk/search/LinkParser.java
+++ web-spyder/src/main/java/ru/alex/st/hh/disk/search/LinkParser.java
@@ -1,7 +1,7 @@
 package ru.alex.st.hh.disk.search;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,15 +14,15 @@ public class LinkParser {
     
     private static final Logger LOGGER = LogManager.getLogger(LinkParser.class);
 
-    private static final Pattern RELATED_LINK = Pattern.compile("href=\".+\"");
-    private static final Pattern ABSOLUT_LINK = Pattern.compile("href=\"http.?:.+\"");
+    private static final Pattern RELATED_LINK = Pattern.compile("href=\".+?\"");
+    private static final Pattern ABSOLUT_LINK = Pattern.compile("href=\"(http|https)://.+?\"");
 
     public LinkParser() {
 
     }
 
-    public List<String> findAndSave(String str) {
-        LinkedList<String> foundLinks = new LinkedList<>();
+    public Set<String> findAndSave(String str) {
+        Set<String> foundLinks = new LinkedHashSet<>();
         Matcher absolutMatcher = ABSOLUT_LINK.matcher(str);
         while (absolutMatcher.find()) {
             foundLinks.add(absolutMatcher.group().split("\"")[1]);
@@ -31,21 +31,6 @@ public class LinkParser {
         while (relatedMatcher.find()) {
             foundLinks.add(relatedMatcher.group().split("\"")[1]);
         }
-        
-
-        // LinkedList<String> result = new LinkedList<>();
-        // InputStream in = url.openStream(); // Ask the URL object to create an
-        // input stream
-        // InputStreamReader isr = new InputStreamReader(in); // Convert the
-        // stream to a reader
-        // SpiderParserCallback cb = new SpiderParserCallback(treenode); //
-        // Create a callback object
-        // ParserDelegator pd = new ParserDelegator(); // Create the delegator
-        // pd.parse(isr,cb,true); // Parse the stream
-        //
-        //
-        // isr.close(); // Close the stream
-
         return foundLinks;
     }
 
@@ -53,17 +38,12 @@ public class LinkParser {
         
         String s1 = "<link rel=\"stylesheet\" href=\"/w/load.php?debug=false&amp;lang=ru&amp;modules=site.styles&amp;only=styles&amp;skin=vector\"/>";
         String s2 = "<link rel=\"canonical\" href=\"https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0\"/>";
+        String s3 = "<a href=\"/wiki/%D0%9C%D1%83%D0%B6%D1%81%D0%BA%D0%BE%D0%B9_%D1%80%D0%BE%D0%B4\" title=\"Мужской род\">мужского рода</a> выделяется подкласс имён — названий лиц мужского пола, противопоставленный всем прочим существительным. Помимо общей ";
+
         LinkParser linkParser = new LinkParser();
         LOGGER.info(linkParser.findAndSave(s2));
         LOGGER.info(linkParser.findAndSave(s1));
-        
-        
-        
-//        <noscript><link rel="stylesheet" href="/w/load.php?debug=false&amp;lang=ru&amp;modules=noscript&amp;only=styles&amp;skin=vector"/></noscript>
-//        <meta name="generator" content="MediaWiki 1.29.0-wmf.21"/>
-//        <meta name="referrer" content="origin-when-cross-origin"/>
-//        <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Robert_Edward_Lee.jpg/1200px-Robert_Edward_Lee.jpg"/>
-
+        LOGGER.info(linkParser.findAndSave(s3));
         
     }
 
---
+++ web-spyder/src/test/java/ru/alex/st/hh/disk/search/LinkParserTest.java
@@ -0,0 +1,43 @@
+package ru.alex.st.hh.disk.search;
+
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class LinkParserTest {
+
+    private LinkParser parser = new LinkParser();
+
+    @Test
+    public void testLinkParser() {
+        String s = "<p><b><a href=\"/wiki/%D0%98%D0%BC%D1%8F_%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D0%B2_%D0%BC%D0%B0%D0%BA%D0%B5%D0%B4%D0%BE%D0%BD%D1%81%D0%BA%D0%BE%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5\" title=\"Имя существительное в македонском языке\">Имя существительное в македонском языке</a></b> "
+                        + "(<a href=\"/wiki/%D0%9C%D0%B0%D0%BA%D0%B5%D0%B4%D0%BE%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B7%D1%8B%D0%BA\" title=\"Македонский язык\">макед.</a> "
+                        + "<span lang=\"mk\" style=\"font-style: italic\" xml:lang=\"mk\">именка во македонскиот јазик</span>) — "
+                        + "<a href=\"/wiki/%D0%97%D0%BD%D0%B0%D0%BC%D0%B5%D0%BD%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%81%D0%BB%D0%BE%D0%B2%D0%B0\" title=\"Знаменательные слова\">знаменательная</a> "
+                        + "<a href=\"/wiki/%D0%A7%D0%B0%D1%81%D1%82%D1%8C_%D1%80%D0%B5%D1%87%D0%B8\" title=\"Часть речи\">часть речи</a> "
+                        + "<a href=\"/wiki/%D0%9C%D0%B0%D0%BA%D0%B5%D0%B4%D0%BE%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B7%D1%8B%D0%BA\" title=\"Македонский язык\">македонского языка</a>, называющая предметы и одушев­лён­ные существа. Характеризуется "
+                        + "<a href=\"/wiki/%D0%93%D1%80%D0%B0%D0%BC%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F\" title=\"Грамматическая категория\">грамматическими категориями</a> "
+                        + "<a href=\"/wiki/%D0%A0%D0%BE%D0%B4_(%D0%BB%D0%B8%D0%BD%D0%B3%D0%B2%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0)\" title=\"Род (лингвистика)\">рода</a>, "
+                        + "<a href=\"/wiki/%D0%93%D1%80%D0%B0%D0%BC%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%BE\" title=\"Грамматическое число\">числа</a> и "
+                        + "<span class=\"iw plainlinks\" data-title=\"Категория определённости — неопределённости\" data-lang=\"en\" data-lang-name=\"англ.\">"
+                        + "<a href=\"/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F_%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D1%91%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%E2%80%94_%D0%BD%D0%B5%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D1%91%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8&amp;action=edit&amp;redlink=1\" class=\"new\" title=\"Категория определённости — неопределённости (страница отсутствует)\">определённости — неопре­де­лённости</a>"
+                        + "<sup class=\"iw__note noprint\" style=\"font-style:normal; font-weight:normal;\">"
+                        + "<a href=\"https://en.wikipedia.org/wiki/Definiteness\" class=\"extiw\" title=\"en:Definiteness\">"
+                        + "<span class=\"iw__tooltip\" title=\"Definiteness — версия статьи «Категория определённости — неопределённости» на английском языке\">[en]</span></a></sup></span>. Среди македонских "
+                        + "<a href=\"/wiki/%D0%98%D0%BC%D1%8F_%D1%81%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5\" title=\"Имя существительное\">существительных</a> выделяются "
+                        + "<a href=\"/wiki/%D0%98%D0%BC%D1%8F_%D1%81%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B5\" title=\"Имя собственное\">имена собственные</a>, "
+                        + "<a href=\"/wiki/%D0%98%D0%BC%D1%8F_%D0%BD%D0%B0%D1%80%D0%B8%D1%86%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5\" title=\"Имя нарицательное\">нарицательные</a>, собирательные, вещественные и отвлечённые. Последние три группы образуют класс неисчисляемых существительных. Внутри класса "
+                        + "<a href=\"/wiki/%D0%9C%D1%83%D0%B6%D1%81%D0%BA%D0%BE%D0%B9_%D1%80%D0%BE%D0%B4\" title=\"Мужской род\">мужского рода</a> выделяется подкласс имён — названий лиц мужского пола, противопоставленный всем прочим существительным. Помимо общей "
+                        + "<a href=\"/wiki/%D0%93%D1%80%D0%B0%D0%BC%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D1%84%D0%BE%D1%80%D0%BC%D0%B0\" title=\"Грамматическая форма\">формы</a> "
+                        + "<a href=\"/wiki/%D0%9C%D0%BD%D0%BE%D0%B6%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%BE\" title=\"Множественное число\">множественного числа</a> грамматически релевантны формы счётного и собирательного множественного числа.</p>";
+        Set<String> linkList = parser.findAndSave(s);
+        for (String link : linkList) {
+            System.out.println(link);
+        }
+        System.out.println("size = " + linkList.size());
+        Assert.assertEquals(15, linkList.size());
+    }
+
+}
